b2a033de4da00bb7b078b0da384a683582483208
George <backs159@icloud.com>
2022-01-11 21:37:22 +0500

    Fixed the constraints. Should work on iPod touch 7th

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var    redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var  greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           redLight.alpha = 0.6
+        yellowLight.alpha = 0.6
+         greenLight.alpha = 0.6
+        
+           redLight.layer.cornerRadius = 64
+        yellowLight.layer.cornerRadius = 64
+         greenLight.layer.cornerRadius = 64
+        startButton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func changeColorButton(_ sender: Any) {
+           if redLight.alpha == yellowLight.alpha  &&
+            greenLight.alpha == yellowLight.alpha
+        {
+            redLight.alpha = 1
+               startButton.setTitle("NEXT", for: .normal)
+        } else if redLight.alpha == 1 {
+               redLight.alpha = 0.6
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.6
+             greenLight.alpha = 1
+            startButton.setTitle("TURN OFF", for: .normal)
+        } else if greenLight.alpha == 1 {
+             greenLight.alpha = 0.6
+             startButton.setTitle("START", for: .normal)
+        }
     }
 
+    
 
 }
 
