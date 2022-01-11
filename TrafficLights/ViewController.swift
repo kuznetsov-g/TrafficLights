6eca6ab7ae2f5ef39304715d90087115ae17c1dc
George <backs159@icloud.com>
2022-01-12 01:10:10 +0500

    Add comment Add logic for StartButton and change radius for views

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -14,39 +14,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var  greenLight: UIView!
     @IBOutlet weak var startButton: UIButton!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-           redLight.alpha = 0.6
-        yellowLight.alpha = 0.6
-         greenLight.alpha = 0.6
-        
-           redLight.layer.cornerRadius = 64
-        yellowLight.layer.cornerRadius = 64
-         greenLight.layer.cornerRadius = 64
+
         startButton.layer.cornerRadius = 10
-    }
+        startButton.setTitle("START", for: .normal)
+        turnOffLights()
+           }
+
     
     @IBAction func changeColorButton(_ sender: Any) {
-           if redLight.alpha == yellowLight.alpha  &&
-            greenLight.alpha == yellowLight.alpha
-        {
-            redLight.alpha = 1
-               startButton.setTitle("NEXT", for: .normal)
-        } else if redLight.alpha == 1 {
-               redLight.alpha = 0.6
-            yellowLight.alpha = 1
-        } else if yellowLight.alpha == 1 {
-            yellowLight.alpha = 0.6
-             greenLight.alpha = 1
-            startButton.setTitle("TURN OFF", for: .normal)
-        } else if greenLight.alpha == 1 {
-             greenLight.alpha = 0.6
-             startButton.setTitle("START", for: .normal)
+           redLight.alpha == 1 ? turnOnLight(color: "Yellow", buttonTitle: "NEXT")     :
+        yellowLight.alpha == 1 ? turnOnLight(color: "Green",  buttonTitle: "TURN OFF") :
+         greenLight.alpha == 1 ? turnOnLight(color: "",       buttonTitle: "START")    :
+            turnOnLight(color: "Red", buttonTitle: "NEXT")
+    }
+    
+    func turnOffLights() {
+        redLight.alpha = 0.6
+     yellowLight.alpha = 0.6
+      greenLight.alpha = 0.6
+       
+        redLight.layer.cornerRadius =    redLight.frame.height / 2
+     yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+      greenLight.layer.cornerRadius =  greenLight.frame.height / 2
+        /*
+         вообще эти строки я должен был добавить во viewDidLoad,
+         но почему-то они плохо влияют на светильники и сперва они не круглые на экранах
+         iPod Touch 7 / iPhone SE
+         с iPhone 11 Pro Max изначально все в порядке
+         
+         Предполагаю, что при загрузке приложения значение  .frame.height берется от значения вьюшки без форматирования под экран девайса.
+         На этом мои полномочия все
+         */
+    }
+    
+    func turnOnLight (color: String, buttonTitle: String) {
+        turnOffLights()
+        startButton.setTitle(buttonTitle, for: .normal)
+        switch color {
+        case "Red":       redLight.alpha = 1
+        case "Yellow": yellowLight.alpha = 1
+        case "Green":   greenLight.alpha = 1
+        default:
+            turnOffLights()
         }
     }
-
-    
-
 }
+
 
